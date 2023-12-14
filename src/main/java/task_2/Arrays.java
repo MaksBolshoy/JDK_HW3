1a5a3fe5388cfc09643356d7be85b7554b61e195
maksb <maksbolshy@mail.ru>
2023-12-14 20:49:05 +0300

    Vernoe resh

--- src/main/java/task_2/Arrays.java
+++ src/main/java/task_2/Arrays.java
@@ -1,2 +1,17 @@
-package task_2;public class Arrays {
+package task_2;
+
+import java.util.ArrayList;
+
+/*
+- Напишите обобщенный метод compareArrays(),
+который принимает два массива и возвращает true, если они одинаковые, и false в противном случае.
+Массивы могут быть любого типа данных, но должны иметь одинаковую длину и содержать элементы одного типа.
+ */
+public class Arrays {
+
+    public static <T> boolean compareArrays(T[] arr, T[] arr2) {
+        boolean  isTheLengthsEquals = arr.length == arr2.length;
+        boolean isTheClaasEquals = arr.getClass().getName().equals(arr2.getClass().getName());
+        return isTheLengthsEquals && isTheClaasEquals;
+    }
 }
--- src/main/java/task_3/Main.java
+++ src/main/java/task_3/Main.java
@@ -1,2 +1,11 @@
-package task_3;public class Main {
+package task_3;
+
+public class Main {
+    public static void main(String[] args) {
+        Pair pair = new Pair("element1", 2);
+
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+        System.out.println("\n" + pair + "\n");
+    }
 }
